553b4f5854710be935753b1573dc682063d6262f
Danirie <emenzo.danirie22@gmail.com>
2025-10-20 14:37:18 -0400

    Final

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -18,7 +18,6 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleContinueShopping = (e) => {
     e.preventDefault();
-    onContinueShopping();
   };
 
   const handleIncrement = (item) => {
@@ -64,7 +63,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+      <button className="get-started-button" onClick={onContinueShopping}>Continue Shopping</button>
         <br />
         <button className="get-started-button1">Checkout</button>
       </div>
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -23,11 +23,12 @@ export const CartSlice = createSlice({
         state.items = state.items.filter(item => item.name !== nameToRemove);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const item = state.items.find(item => item.name === name);
-        if (item && quantity > 0) {
-            item.quantity = quantity;
-        }
+        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+// Find the item in the cart that matches the given name
+const itemToUpdate = state.items.find(item => item.name === name);
+if (itemToUpdate) {
+  itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+}
     
     },
   },
